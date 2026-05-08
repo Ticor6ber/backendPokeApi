642db95cb43330829b74d2059f0e571043f802f6
Angel P <angelxavier874@gmail.com>
2026-05-08 12:26:02 -0500

    Improve MongoDB connection logging
    Enhance MongoDB connection error handling.

--- NoSQL/db.js
+++ NoSQL/db.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB conectado ✅"))
-.catch(err => console.log(err));
+.then(() => {
+  console.log("MongoDB conectado ✅");
+})
+.catch((err) => {
+  console.log("Error MongoDB:");
+  console.log(err);
+});
 
 module.exports = mongoose;
