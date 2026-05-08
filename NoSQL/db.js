353343fe74e013af40e631efc520afb3b3aa2d93
Angel P <angelxavier874@gmail.com>
2026-05-08 12:19:43 -0500

    Update MongoDB connection to use environment variable

--- NoSQL/db.js
+++ NoSQL/db.js
@@ -1,12 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://angelxavier874_db_user:Ryly7XAF2UqpubJy@pokedb.o3ddyje.mongodb.net/?appName=PokeDB",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB conectado ✅"))
 .catch(err => console.log(err));
 
